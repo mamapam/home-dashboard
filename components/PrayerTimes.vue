f0e134e34c51b1e91c6ee32b6efdd1f1ccd612bd
Moeen M <mamapam.dev@gmail.com>
2024-08-10 15:49:35 -0400

    fix hydration error

---
+++ components/PrayerTimes.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import TheCard from './TheCard.vue';
+</script>
+
+<template>
+  <TheCard>
+    <div>
+      <h3>Prayer Times</h3>
+      <table>
+        <tr>
+          <td></td>
+          <th scope="col">Athan</th>
+          <th scope="col"></th>
+        </tr>
+      </table>
+    </div>
+  </TheCard>
+</template>
